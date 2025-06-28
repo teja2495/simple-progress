506a45be51fa6d0e4552e6d06bf4fe2a65f74ee6
Teja Karlapudi <teja2495@gmail.com>
2025-06-27 20:25:02 -0400

    feat: update gitignore and gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,18 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file(project.property("MYAPP_UPLOAD_STORE_FILE") as String)
+            storePassword = project.property("MYAPP_UPLOAD_STORE_PASSWORD") as String
+            keyAlias = project.property("MYAPP_UPLOAD_KEY_ALIAS") as String
+            keyPassword = project.property("MYAPP_UPLOAD_KEY_PASSWORD") as String
+        }
+    }
+
     buildTypes {
         release {
+            signingConfig = signingConfigs["release"]
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
