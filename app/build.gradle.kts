ccd59987b9cd3c1ab2bc85c3b0232b70cb01131d
Teja Karlapudi <teja2495@gmail.com>
2025-12-29 15:00:15 -0500

    update version code & gitignore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.tk.simpleprogress"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
+        versionCode = 4
         versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
